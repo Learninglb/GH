9b568bdd3051022f798728138fca1ebe91e40c68
Lori Blasing <l.blasing@flathead.coop>
2020-02-12 15:48:35 -0700

    Chapter 7 final

--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -25,7 +25,7 @@ def index():
             'body': 'Beautiful day in Kalispell!'
         }
     ]
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('index.html', title='Home Page', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -84,7 +84,7 @@ def before_request():
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
