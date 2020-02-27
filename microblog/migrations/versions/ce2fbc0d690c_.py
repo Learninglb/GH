dc1d408ea57e8404dd230af55775b26e98854875
Lori Blasing <l.blasing@flathead.coop>
2020-02-27 13:29:18 -0700

    Single db again

---
+++ microblog/migrations/versions/ce2fbc0d690c_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ce2fbc0d690c
+Revises: ae346256b650, 45885a3b2da1
+Create Date: 2020-02-27 13:23:40.579104
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ce2fbc0d690c'
+down_revision = ('ae346256b650', '45885a3b2da1')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
