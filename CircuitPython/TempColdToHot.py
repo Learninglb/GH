1853f8fff10e087fc1c3d01fda1b939230ffb172
Lori Blasing <l.blasing@flathead.coop>
2020-02-27 13:13:38 -0700

    update colors

--- CircuitPython/TempColdToHot.py
+++ CircuitPython/TempColdToHot.py
@@ -5,11 +5,11 @@ cp.pixels.auto_write = False
 cp.pixels.brightness = 0.01
 
 RED = (255, 0, 0)
-YELLOW = (255, 150, 0)
+YELLOW = (255, 255, 0)
 GREEN = (0, 255, 0)
-CYAN = (0, 255, 255)
 BLUE = (0, 0, 255)
-PURPLE = (180, 0, 255)
+PURPLE = (153, 0, 255)
+PINK = (255, 102, 255)
 WHITE = (255, 255, 255)
 GREY = (153, 153, 153)
 
@@ -17,7 +17,7 @@ while True:
     vTemp = cp.temperature
     for i in range(10):
         if vTemp <= 20:
-            cp.pixels.fill(PURPLE)
+            cp.pixels.fill(GREY)
         else:
             if vTemp <= 22:
                 cp.pixels.fill(BLUE)
@@ -26,9 +26,12 @@ while True:
                     cp.pixels.fill(GREEN)
                 else:
                     if vTemp <= 26:
-                        cp.pixels.fill(YELLOW)
+                        cp.pixels.fill(PINK)
                     else:
-                        if vTemp > 26:
-                            cp.pixels.fill(RED)
+                        if vTemp <= 28:
+                            cp.pixels.fill(YELLOW)
+                        else:
+                            if vTemp > 28:
+                                cp.pixels.fill(RED)
         cp.pixels.show()
         time.sleep(0.05)
