221435295f6aa80274fdfd75069eba85ce4763a2
Lori Blasing <l.blasing@flathead.coop>
2020-04-16 15:46:47 -0600

    created upload form

--- my_webapps/app/routes.py
+++ my_webapps/app/routes.py
@@ -1,6 +1,21 @@
+from flask import render_template, request, redirect
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired
+from werkzeug.utils import secure_filename
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html', title='Home')
+
+@app.route('/upload', methods=["GET", "POST"])
+def upload():
+    if request.method == "POST":
+
+        if request.files:
+            text = request.files['text']
+            print (text)
+            return redirect(request.url)
+
+    return render_template('upload.html', title='Upload')
