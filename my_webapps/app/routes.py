484f786d881994fbf10c11b79af9f2d558754bec
Lori Blasing <l.blasing@flathead.coop>
2020-04-20 14:26:56 -0600

    validates and uploads file

--- my_webapps/app/routes.py
+++ my_webapps/app/routes.py
@@ -1,3 +1,4 @@
+import os
 from flask import render_template, request, redirect
 from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileRequired
@@ -9,13 +10,35 @@ from app import app
 def index():
     return render_template('index.html', title='Home')
 
+def allowed_file(filename):
+    if not "." in filename:
+        return False
+    ext = filename.rsplit(".", 1)[1]
+
+    if ext.upper() in app.config["FILES_ALLOWED"]:
+        return True
+    else:
+        return False
+
 @app.route('/upload', methods=["GET", "POST"])
 def upload():
     if request.method == "POST":
 
         if request.files:
             text = request.files['text']
-            print (text)
+
+            if text.filename=="":
+                print("File must have a name.")
+                return redirect(request.url)
+            
+            if not allowed_file(text.filename):
+                print("That file extension is not allowed.")
+                return redirect(request.url)
+            else:
+                filename = secure_filename(text.filename)
+                text.save(os.path.join(app.config["FILE_UPLOAD"], filename))
+
+            print("file saved")
             return redirect(request.url)
 
     return render_template('upload.html', title='Upload')
--- my_webapps/config.py
+++ my_webapps/config.py
@@ -2,4 +2,5 @@ import os
 
 class Config(object):
     SECRET_KEY= os.environ.get('SECRET_KEY') or 'you-might-never-guess'
-    
+    FILE_UPLOAD = "c:/GH/my_webapps/app/uploads"
+    FILES_ALLOWED = ["TXT", "CSV"]
