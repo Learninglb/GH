4c40bc372f9ecb569836c51e79cc9be0ed28090d
Lori Blasing <l.blasing@flathead.coop>
2020-05-01 15:07:59 -0600

    lesson 5, nav and templates

---
+++ my_webapps/app/admin_views.py
@@ -0,0 +1,11 @@
+from app import app
+from flask import render_template
+
+
+@app.route('/admin/dashboard')
+def admin_dashboard():
+    return render_template('admin/dashboard.html', title='Admin-Dashboard')
+
+@app.route('/admin/profile')
+def admin_profile():
+    return render_template('admin/profile.html', title='Admin-Profile')
